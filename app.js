dd6fa2da57aadf0995e6d728e0fba4af80ad4b00
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-12 10:23:40 +0200

    Add script to handle render new message

---
+++ app.js
@@ -0,0 +1,50 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
+messagesSection.classList.remove('show');
+
+const addMessage = (author, content) => {
+
+  const message = `<li class="${author == userName ? "message message--received message--self": "message message--received"}">
+  <h3 class="message__author">${author == userName ? "You" : author}</h3>
+  <div class="message__content">
+    ${content}
+  </div>
+  </li>`;
+
+  messagesList.insertAdjacentHTML('beforeend', message);
+}
+
+
+const login = (event) => {
+  event.preventDefault();
+
+  if (userNameInput.value) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+
+  } else {
+    alert('Make sure you write your name.')
+  }
+};
+
+const sendMessage = (event) => {
+  event.preventDefault();
+
+  if (messageContentInput.value) {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  } else {
+    alert('Make sure you write your message.');
+  }
+}
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
